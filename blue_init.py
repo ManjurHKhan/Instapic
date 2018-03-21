c2ca489a91d13b5f9e93c925c61d17c15e6f8cb5
Lise Ho <koprty@users.noreply.github.com>
2018-03-21 18:43:06 -0400

    debugging begins here

--- blue_init.py
+++ blue_init.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from api import mod as api_routes
-from site import mod as site_routes
+from Instapic.api import mod as api_routes
+from Instapic.site import mod as site_routes
 
 app.register_blueprint(site_routes, url_prefix ='/site')
-app.register_blueprint(api_routes))
+app.register_blueprint(api_routes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
