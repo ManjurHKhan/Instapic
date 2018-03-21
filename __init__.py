e5991b6106a098726f88b202b1c15097d4162c6d
Lise Ho <koprty@users.noreply.github.com>
2018-03-21 18:19:18 -0400

    fixed small bug.. :/

--- __init__.py
+++ __init__.py
@@ -8,5 +8,5 @@ from blue.site.routes import mod
 app.register_blueprint(site.routes.mod, url_prefix ='/site')
 app.register_blueprint(api.routes.mod)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
