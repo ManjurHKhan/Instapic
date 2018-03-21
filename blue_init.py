ce8f4ad1c27a5c6c3cdbe9386f3950be6bb84f2f
Lise Ho <koprty@users.noreply.github.com>
2018-03-21 18:38:39 -0400

    getting rid of bad blue imports

--- blue_init.py
+++ blue_init.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from blue.api.routes import mod
-from blue.site.routes import mod
+from api import mod as api_routes
+from site import mod as site_routes
 
-app.register_blueprint(site.routes.mod, url_prefix ='/site')
-app.register_blueprint(api.routes.mod)
+app.register_blueprint(site_routes, url_prefix ='/site')
+app.register_blueprint(api_routes))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
