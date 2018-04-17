d8344d1ff8d1d150efe78bdee50eb53c75a8b73f
Lise Ho <koprty@users.noreply.github.com>
2018-04-17 01:29:17 -0400

    like number updates with like

--- insta/api.py
+++ insta/api.py
@@ -15,6 +15,7 @@ import smtplib
 ## debugging tools
 import traceback
 
+urltoFiles = ""
 
 # TODO we have to do something about the conn. We should really call it only inside try.
 ######################
@@ -153,7 +154,7 @@ def login():
     user_cookie = session.get("userID")
     if (user_cookie != None):
         cur = conn.cursor()
-        query = "SELECT username FROM USERS where username='%s' and validated is True;"
+        query = "SELECT username FROM USERS where username=%s and validated is True;"
         cur.execute(query, (user_cookie,))
         rez = cur.fetchone()
         if rez != None:
@@ -290,11 +291,12 @@ def add_items():
     if (user_cookie != None):
         if (request.headers.get('Content-Type') == 'application/json'):
             data = request.get_json(silent=True)
-
             if(data != None):
                 try:
                     content = None
                     child_type = None
+                    parent = None
+                    media = []
                     if "content" in data:
                         content = data['content'].rstrip() if data['content'].rstrip() != "" else None # we do not need to remove starting spaces
                     if "childType" in data:
@@ -305,23 +307,38 @@ def add_items():
                     if(content == None):
                         return jsonify(status="error", error="Content is null")
                     postid = hashlib.md5(str(time.time()).encode('utf-8')).hexdigest()
-                
-                    logger.debug('additem-content 1: %s',data['content'])
-                    logger.debug('additem-content 2: %s',content)
-
-                    # logger.debug('conn:%s', conn)
+                    
                     cur = conn.cursor()
-                    # content = content.replace("'", "''").encode('UTF-8')
-                    query = "INSERT INTO posts(username, postid, content, retweet) VALUES (%s, %s, %s, %s);"
-                    logger.debug('additem-content SQL 3: %s', query, (user_cookie, postid, content, child_type == 'retweet', ))
-                    # logger.debug("query: %s", query % (user_cookie, postid, content, child_type == 'retweet'))
-                    cur.execute(query, (user_cookie, postid, content, child_type == 'retweet', ))
+                    parent = data["parent"].rstrip() if data["parent"].rstrip() != "" else None
+                    
+                    if parent != None:
+                        query = "INSERT INTO posts(username, postid, content, child_type, parent_id) VALUES (%s, %s, %s, %s,%s);"
+                        cur.execute(query, (user_cookie, postid, content, child_type, parent ))
+                        #cur.execute(query, (user_cookie, postid, content, child_type == 'retweet', parent ))
+                    else:
+                        #logger.debug('additem-content 1: %s',data['content'])
+                        #logger.debug('additem-content 2: %s',content)
+                        query = "INSERT INTO posts(username, postid, content, child_type) VALUES (%s, %s, %s, %s);"
+                        logger.debug('additem-content SQL 3: %s', query, (user_cookie, postid, content, child_type, ))
+                        # logger.debug("query: %s", query % (user_cookie, postid, content, child_type == 'retweet'))
+                        cur.execute(query, (user_cookie, postid, content, child_type, ))
+
+                    if "media" in data:
+                        media = data["media"]
+                        print (media)
+                        print (media[0])
+                        # Making sure if media exists first
+                        # Skipping to save time
+                        for mediaid in media:
+                            query = "INSERT INTO user_media (username, postid, mediaid) VALUES (%s, %s, %s);"
+                            cur.execute(query, (user_cookie, postid, mediaid,))
+
                     cur.close()
                     conn.commit()
                     conn.close()
                     return jsonify(status="OK", id=postid)
                 except Exception as e:
-                    logger.debug('additem: somthing went wrong: %s',e)
+                    logger.debug('additem: something went wrong %s',e)
                     logger.debug(traceback.format_exc())
                     if (cur != None):
                         cur.close()
@@ -334,7 +351,7 @@ def add_items():
     return jsonify(status="error", error="Not logged in")
 
 
-@mod.route("/item/<id>", methods=["GET"]) # IDK why this was POST
+@mod.route("/item/<id>", methods=["GET"])
 def get_item(id):
     conn = psycopg2.connect(**params)
     curr = None
@@ -344,7 +361,7 @@ def get_item(id):
         try:
             logger.debug('conn:%s', conn)
             cur = conn.cursor()
-            query = "SELECT * FROM posts WHERE postid = %s;"
+            query = "SELECT username, postid, date, content,  child_type, parent_id, retweet_cnt, numliked FROM posts WHERE postid = %s;"
             logger.debug("get item query:%s", query % (str(id)))
             cur.execute(query, (str(id), ))
             i = cur.fetchone()
@@ -355,10 +372,18 @@ def get_item(id):
             # logger.debug("get_item - time: %s", i[2])
             # logger.debug("get_item -", (i[2].split('.')))
             # logger.debug("get_item -", (str(i[2]).split('.')))
-
             # logger.debug("get_item -", int(time.mktime(time.strptime(str(i[2]).split('.')[0], '%Y-%m-%dT%H:%M:%S'))))
+            print(i)
 
-            item = {'id':i[1], 'username':i[0], 'property':{'likes':i[7]}, 'retweeted':i[6], 'content':i[3], 'timestamp': int(time.mktime(time.strptime(str(i[2]).split('.')[0], '%Y-%m-%d %H:%M:%S')))}
+            item = {'id':i[1], 'username':i[0], 'property':{'likes':i[7]}, 'retweeted':i[6], 'content':i[3], 'timestamp': int(time.mktime(time.strptime(str(i[2]).split('.')[0], '%Y-%m-%d %H:%M:%S'))), 'childType':i[4], 'parent':i[5] }
+            query="SELECT mediaid from user_media where username=%s and postid=%s;";
+            print (query, (user_cookie, str(id)))
+            cur.execute(query, (user_cookie, str(id),) )   
+            rez = cur.fetchall()
+            print (query)
+            print (rez)
+
+
             cur.close()
             conn.commit()
             conn.close()
@@ -413,6 +438,28 @@ def search():
                     q_string = "%%%s%%" % (data["q"])
                     query += "AND content LIKE %s "
                     q_data += (q_string,)
+                
+                # if "rank" in data:
+                #     if data["hasMedia"].rstrip().capitalize()=="True":
+                #         q_string = "%%%s%%" % (data["q"])
+                #         query += "AND content LIKE %s "
+                #         q_data += (q_string,)
+                #  if "parent" in data:
+                #     if data["parent"].rstrip() != None:
+                #         q_string = "%%%s%%" % (data["q"])
+                #         query += "AND content LIKE %s "
+                #         q_data += (q_string,)
+                #  if "replies" in data:
+                #     if data["replies"].rstrip().capitalize()=="True":
+                #         q_string = "%%%s%%" % (data["q"])
+                # #         query += "AND childType <> "
+                # #         q_data += (q_string,)
+
+                # if "hasMedia" in data:
+                #     if data["hasMedia"].rstrip().capitalize()=="True":
+                #         q_string = "%%%s%%" % (data["q"])
+                #         query += "AND content LIKE %s "
+                #         q_data += (q_string,)
                 # if username == None and not following:
                 #     query += "AND username = %s "
                 #     q_data += (user_cookie,)
@@ -445,7 +492,7 @@ def search():
                     conn.close()
                     return jsonify(status="OK", items=ret_items)
                 except Exception as e:
-                    logger.debug('login: error  %s', e)
+                    logger.debug('search: error  %s', e)
                     logger.debug(traceback.format_exc())
                     if (cur != None):
                         cur.close()
@@ -556,7 +603,6 @@ def user_followers(username):
         logger.debug(traceback.format_exc())
         return jsonify(status="error",error="Some DB connection failed probably")
 
-
 @mod.route("/user/<username>/following", methods=["GET"])
 def users_user_is_following(username):
     try:
@@ -655,6 +701,109 @@ def user_follow():
         logger.error('follow: Error  %s', e)
         logger.debug(traceback.format_exc())
         return jsonify(status="error",error="Some DB connection failed probably while trying to follow")
+
+@mod.route("/item/<id>/like", methods=["POST"])
+def post_like(id):
+    post_id = id
+    logger.debug('top follow: starting endpoint eval ')
+    try:
+        conn = psycopg2.connect(**params)
+        cur = None
+        user_cookie = session.get("userID")
+        if (user_cookie != None):
+            cur = conn.cursor()
+            # check to make sure user is in the database
+            query = "SELECT username FROM USERS where username=%s and validated is True;"
+            cur.execute(query, (user_cookie, ))
+            rez = cur.fetchone()
+            if rez == None:
+                # login
+                cur.close()
+                conn.close()
+                return jsonify(status="OK")
+        if (request.headers.get('Content-Type') == 'application/json'):
+            data = request.get_json(silent=True)
+            if (data != None):
+                like = True
+                if "like" in data: 
+                    like = data["like"].strip().capitalize() == "True"
+               
+                if like:
+                    #like
+                    query = "INSERT INTO likes (username, postid) VALUES (%s , %s);"
+                    cur.execute(query, (user_cookie, post_id,))
+                    query = "UPDATE posts set numliked = numliked+1 where username=%s and postid=%s;"
+                    #query = "UPDATE posts set numliked = (select count(*) from likes where username=%s and postid =%s);"
+                    cur.execute(query, (user_cookie, post_id,))
+                else: 
+                    # query = "SELECT * FROM likes where username=%s and postid=%s;"
+                    # cur.execute(query, (user_cookie, post_id,))
+                    # rez = cur.fetchone()
+                    # if rez != None and len(rez) > 0:
+                    query = "DELETE FROM likes where username=%s and postid=%s RETURNING *"
+                    #" and exists(select * from likes where username=%s and postid=%s );"
+                    cur.execute(query, (user_cookie, post_id,))
+                    #cur.execute(query, (user_cookie, post_id,user_cookie, post_id,))
+                    rez = cur.fetchall()
+                    if len(rez) > 0: 
+                        query = "UPDATE posts set numliked = numliked-1 where username=%s and postid=%s;"
+                        #query = "UPDATE posts set numliked = (select count(*) from likes where username=%s and postid =%s);"
+                        cur.execute(query, (user_cookie, post_id,))
+                    else:
+                        # nothing was unliked
+                        cur.close()
+                        conn.commit()
+                        conn.close()
+                        return jsonify(status="error",msg="Could not unlike a post that was not liked by user in the first place")
+                cur.close()
+                conn.commit()
+                conn.close()
+                msg = "Liked a post for %s"%user_cookie if like else "Unliked a post for %s"%user_cookie
+                return jsonify(status="OK",msg=msg)
+        return jsonify(status="error",error="Invalid request - send json please.")
+        
+    except Exception as e:
+        logger.error('follow: Error  %s', e)
+        logger.debug(traceback.format_exc())
+        return jsonify(status="error",error="Some DB connection failed probably while trying to follow")
+
+
+@mod.route("/media/<id>", methods=["GET"])
+def get_media(id):
+    domain_get_media = urltoFiles + "/%s"%(id)
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace('old-domain.com', urltoFiles),
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    # forward request to the file storage machine
+    pass
+
+@mod.route("/addmedia", methods=["POST"])
+def add_media():
+    try:
+        conn = psycopg2.connect(**params)
+        cur = None
+        user_cookie = session.get("userID")
+        if (user_cookie != None):
+            cur = conn.cursor()
+            # check to make sure user is in the database
+            query = "SELECT username FROM USERS where username=%s and validated is True;"
+            cur.execute(query, (user_cookie, ))
+            rez = cur.fetchone()
+        #forward request to the file storage machine
+        query = "INSERT INTO media (username, mediaid) VALUES (%s, %s);"
+        cur.execute(query, (user_cookie, mediaid,))
+    except Exception as e:
+        logger.error('addmedia: Error  %s', e)
+        logger.debug(traceback.format_exc())
+        return jsonify(status="error",error="Some DB connection failed probably while trying to follow")
+
+    
+
 
 
                    
