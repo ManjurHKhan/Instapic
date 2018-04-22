31fc89b1ce07f23ce81601af254679db13ade67e
Lise Ho <koprty@users.noreply.github.com>
2018-04-22 04:22:06 -0400

    let's spell things correctly now... cur

--- insta/api.py
+++ insta/api.py
@@ -71,8 +71,8 @@ def send_delete_node(postid):
     return f.getcode() == 200 # return true if deleted node
 
 
-def add_item_thread(user_cookie,postid, data):
-    curr = None
+def add_item_thread(user_cookie, postid, data):
+    cur = None
     conn = None
     try:
         content = None
