c595bab6e27bf5070d0bdee2e65af47304291b4f
Manjur Khan <manjur.h.khan@gmail.com>
2018-04-24 06:41:45 -0400

    added body to the search dictionary

--- insta/api.py
+++ insta/api.py
@@ -159,7 +159,7 @@ def hello2():
                         "query": {
                             "bool": {
                                 "must": [
-                                {  "match": { "content": "KWdeemglJxiHVrVc" }}
+                                {  "match": { "body" :{ "content": "KWdeemglJxiHVrVc" }}}
                                 #,
                                 # { "range": { "timestamp":  {
                                 #             "gte" : timestamp,
@@ -655,7 +655,7 @@ def search():
                         "query": {
                             "bool": {
                                 "must": [
-                                {  "match": { "content": "%s"%(data["q"].replace("\n","")) }}
+                                {  "match": { "body" : {"content": "%s"%(data["q"].replace("\n",""))} }}
                                 #,
                                 # { "range": { "timestamp":  {
                                 #             "gte" : timestamp,
